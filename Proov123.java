0330f3ca916b7390f42bc10d4f272148733ce35f
Kristel Roosimaa <krisroos@greeny.cs.tlu.ee>
2017-02-07 15:06:14 +0200

    Grupitöö

---
+++ Proov123.java
@@ -0,0 +1,7 @@
+public class Proov123{
+    public static void main (String[] args){
+        Koonus test1=new Koonus(2,3,5);
+        System.out.printf("Koonuse ruumala on %.2f ",test1.ruumala());
+        System.out.println("Koonuse pindala on "+test1.pindala());
+    }
+}
