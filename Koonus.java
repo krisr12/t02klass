9a5881e285a190a12e1b3376c632530ae0649e90
Kristel Roosimaa <krisroos@greeny.cs.tlu.ee>
2017-02-07 15:34:12 +0200

    Kristel Roosimaa kodutöö2

--- Koonus.java
+++ Koonus.java
@@ -1,12 +1,14 @@
 public class Koonus{
-    double r, h ,m;
-    public Koonus(double raadius, double k6rgus, double moodustaja){
+    double r, h, m, m2;
+    public Koonus(double raadius, double k6rgus, double moodustaja, double mass){
         if(raadius<=0){throw new RuntimeException("Sobimatu raadius");}
         if(k6rgus<=0){throw new RuntimeException("Sobimatu korgus");}
         if(moodustaja<=0){throw new RuntimeException("Sobimatu korgus");}
+		if(mass<=0){throw new RuntimeException("Sobimatu mass");}
         r=raadius;
         h=k6rgus;
         m=moodustaja;
+		m2=mass;
     }
     public double rpindala(){
         return 3.14*(r*r);
@@ -17,4 +19,7 @@ public class Koonus{
     public double pindala(){
         return 3.14* r *(r+m);
     }
+	public double tihedus () {
+		return m2/(1.0/3*3.14*r*r*h);
+	}
 }
--- Proov123.java
+++ Proov123.java
@@ -1,7 +1,8 @@
 public class Proov123{
     public static void main (String[] args){
-        Koonus test1=new Koonus(2,3,5);
+        Koonus test1=new Koonus(2,3,5,300);
         System.out.printf("Koonuse ruumala on %.2f ",test1.ruumala());
         System.out.println("Koonuse pindala on "+test1.pindala());
+		System.out.printf("Koonuse tihedus on %.2f ",test1.tihedus());
     }
 }
